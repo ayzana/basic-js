c973c7844746800aa89b409c2fa4b14784c78951
Ayzanat Gabieva <aizanka001@gmail.com>
2025-11-09 12:52:39 +0300

    feat: add solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create a repeating string based on the given parameters
@@ -16,11 +16,35 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  if (options.addition === undefined) {
+    options.addition = "";
+  }
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.separator === undefined) {
+    options.separator = "+";
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = "|";
+  }
+  let resultStr = str;
+
+  return (
+    str +
+    ((options.addition + options.additionSeparator)
+      .repeat(options.additionRepeatTimes)
+      .slice(0, -options.additionSeparator.length) +
+      options.separator)
+  )
+    .repeat(options.repeatTimes)
+    .slice(0, -options.separator.length);
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,21 @@ const { NotImplementedError } = require('../lib');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+  let arr2 = arr.filter((el) => el !== -1).sort((a, b) => a - b);
+  let arr3 = [];
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] === -1) {
+      arr3.push(-1);
+    } else {
+      arr3.push(arr2[0]);
+      arr2.splice(0, 1);
+    }
+  }
+  return arr3;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
